4d312c4715106a032bb2f6a0ca28b7cef37c6d03
davidevinavil <davidevinavil@gmail.com>
2018-03-06 18:05:11 +0100

    Split vendor specific and system props

--- BoardConfig.mk
+++ BoardConfig.mk
@@ -28,7 +28,7 @@ TARGET_KERNEL_CONFIG := z2_row_defconfig
 TARGET_KERNEL_CROSS_COMPILE_PREFIX := aarch64-linux-gnu-
 
 # Properties
-TARGET_SYSTEM_PROP += $(DEVICE_PATH)/system.prop
+TARGET_VENDOR_PROP += $(DEVICE_PATH)/vendor.prop
 
 # Inherit from the proprietary version
 -include vendor/zuk/z2_row/BoardConfigVendor.mk
