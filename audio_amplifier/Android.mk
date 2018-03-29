3142ce3386287fbbfbae7310feabe464a4c95105
Cosme Domínguez Díaz <cosme.ddiaz@gmail.com>
2018-03-29 13:02:12 +0200

    audio_amplifier: Remove deprecated kernel header dependency path.

--- audio_amplifier/Android.mk
+++ audio_amplifier/Android.mk
@@ -3,21 +3,19 @@ LOCAL_PATH := $(call my-dir)
 include $(CLEAR_VARS)
 
 LOCAL_C_INCLUDES += \
-     $(call project-path-for,qcom-audio)/hal/msm8974/ \
      $(TARGET_OUT_INTERMEDIATES)/KERNEL_OBJ/usr/include \
-		 hardware/libhardware/include
+     $(call project-path-for,qcom-audio)/hal/msm8974 \
+     hardware/libhardware/include
 
-LOCAL_ADDITIONAL_DEPENDENCIES += $(TARGET_OUT_INTERMEDIATES)/KERNEL_OBJ/usr
-
+LOCAL_ADDITIONAL_DEPENDENCIES := INSTALLED_KERNEL_HEADERS
 LOCAL_SHARED_LIBRARIES := liblog libutils libcutils libdl
+
 LOCAL_CFLAGS := -Wall -Werror -Wno-unused-parameter
 
 LOCAL_SRC_FILES := audio_amplifier.c
 
 LOCAL_MODULE := audio_amplifier.msm8996
-
 LOCAL_MODULE_RELATIVE_PATH := hw
-
 LOCAL_MODULE_TAGS := optional
 
 LOCAL_PROPRIETARY_MODULE := true
